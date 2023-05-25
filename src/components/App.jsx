b703323427aaf868deab0a86410bb63c890b2866
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-05-25 12:51:13 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,8 +52,8 @@ export default function App() {
     if (query === "") {
       return;
     }
-      fetchImgOnQuery();
-  });
+   fetchImgOnQuery();
+  }, [query]);
 
 
   function fetchImgOnQuery() {
